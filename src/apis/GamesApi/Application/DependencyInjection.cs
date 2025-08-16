028e542ad36423d80e3a6770877bd3b8dc58cfab
wosiu6 <wos.patryk3@gmail.com>
2025-08-16 19:29:17 +0100

    add put and delete endpoints for games

--- src/apis/GamesApi/Application/DependencyInjection.cs
+++ src/apis/GamesApi/Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using AutoMapper;
 using GamesApi.Application.Games.Commands.CreateGame;
+using GamesApi.Application.Games.Commands.DeleteGame;
+using GamesApi.Application.Games.Commands.UpdateGame;
 using GamesApi.Application.Games.Queries.GetGames;
 using GamesApi.Domain.Entities;
 using MediatR;
@@ -23,10 +25,23 @@ public static class DependencyInjection
     {
         app.MapPost("/games", async (IMediator mediator, CreateGameCommand request) =>
         {
-            var newGame = await mediator.Send(request);
-            return Results.Created($"games/{newGame}", newGame);
+            var newGameId = await mediator.Send(request);
+            return TypedResults.Created($"games/{newGameId}", newGameId);
+        });
+        
+        app.MapPut("/games/{id:int}", async Task<IResult> (IMediator mediator, int id, UpdateGameCommand request) =>
+        {
+            if (id != request.Id) return TypedResults.BadRequest();
+            await mediator.Send(request);
+            return TypedResults.NoContent();
         });
 
-        app.MapGet("/games", async (IMediator mediator) => Results.Ok(await mediator.Send(new GetGamesQuery())));
+        app.MapDelete("/games/{id:int}", async (IMediator mediator, int id) =>
+        {
+            await mediator.Send(new DeleteGameCommand(id));
+            return TypedResults.NoContent();
+        });
+
+        app.MapGet("/games", async (IMediator mediator) => TypedResults.Ok(await mediator.Send(new GetGamesQuery())));
     }
 }
