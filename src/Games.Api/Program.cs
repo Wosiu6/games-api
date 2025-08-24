97b0810adc507657499a3ce99514226a4e0d7f43
wosiu6 <wos.patryk3@gmail.com>
2025-08-24 14:49:56 +0100

    enhance folder structure add initial tests

---
+++ src/Games.Api/Program.cs
@@ -0,0 +1,46 @@
+using Application;
+using Infrastructure;
+using Infrastructure.Data;
+using Infrastructure.Data.Extensions;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument(configuration, "Identity");
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(configuration);
+
+builder.Services.AddGamesApplicationServices();
+builder.Services.AddGameInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Games Api")
+            .WithTheme(ScalarTheme.Purple)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+
+    app.UseSwaggerUi();
+    app.UseOpenApi();
+
+    await app.InitialiseDatabaseAsync<GamesDbContextInitialiser>();
+}
+
+app.MapGamesEndpoints();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
---
+++ src/Infrastructure/Data/GamesDbContextInitialiser.cs
@@ -0,0 +1,78 @@
+﻿using Domain.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data;
+
+public class GamesDbContextInitialiser(
+    ILogger<GamesDbContextInitialiser> logger,
+    GamesDbContext context) : IDbContextInitialiser
+{
+    public async Task InitialiseAsync()
+    {
+        try
+        {
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+            await SeedAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while initialising the database.");
+            throw;
+        }
+    }
+    public async Task SeedAsync()
+    {
+        try
+        {
+            await TrySeedAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while seeding the database.");
+            throw;
+        }
+    }
+
+    public async Task TrySeedAsync()
+    {
+        if (!context.Games.Any())
+        {
+            var games = new List<Game>
+            {
+                new Game()
+                {
+                    Title = "Cyberpunk 2077",
+                    ReleaseDate = new DateTime(2020, 12, 10, 0, 0, 0, DateTimeKind.Utc),
+                    CreatedOn = DateTime.UtcNow,
+                    UpdatedOn = DateTime.UtcNow
+                },
+                new Game()
+                {
+                    Title = "The Witcher",
+                    ReleaseDate = new DateTime(2007, 10, 26, 0, 0, 0, DateTimeKind.Utc),
+                    CreatedOn = DateTime.UtcNow,
+                    UpdatedOn = DateTime.UtcNow
+                },
+                new Game()
+                {
+                    Title = "The Witcher 2: Assassins of Kings",
+                    ReleaseDate = new DateTime(2011, 5, 17, 0, 0, 0, DateTimeKind.Utc),
+                    CreatedOn = DateTime.UtcNow,
+                    UpdatedOn = DateTime.UtcNow
+                },
+                new Game()
+                {
+                    Title = "The Witcher 3: Wild Hunt",
+                    ReleaseDate = new DateTime(2015, 5, 19, 0, 0, 0, DateTimeKind.Utc),
+                    CreatedOn = DateTime.UtcNow,
+                    UpdatedOn = DateTime.UtcNow
+                }
+            };
+
+            context.Games.AddRange(games);
+            await context.SaveChangesAsync();
+        }
+    }
+}
